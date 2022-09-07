7579b3e0f923b75d09ad570cf2339fc4417313e5
Pablo <zapatagallegojuan@gmail.com>
2022-09-07 16:21:07 -0500

    Agregar archivos de proyecto.

---
+++ CRUD CSV 2019/frmInicio.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace CRUD_CSV_2019
+{
+    partial class frmInicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLeerCSV = new System.Windows.Forms.Button();
+            this.btnModificarCSV = new System.Windows.Forms.Button();
+            this.dgvCSV = new System.Windows.Forms.DataGridView();
+            this.btnCrearCSV = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.txtArchivo = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCSV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLeerCSV
+            // 
+            this.btnLeerCSV.Location = new System.Drawing.Point(987, 284);
+            this.btnLeerCSV.Name = "btnLeerCSV";
+            this.btnLeerCSV.Size = new System.Drawing.Size(75, 23);
+            this.btnLeerCSV.TabIndex = 2;
+            this.btnLeerCSV.Text = "Leer";
+            this.btnLeerCSV.UseVisualStyleBackColor = true;
+            this.btnLeerCSV.Click += new System.EventHandler(this.BtnLeerCSV_Click);
+            // 
+            // btnModificarCSV
+            // 
+            this.btnModificarCSV.Location = new System.Drawing.Point(987, 366);
+            this.btnModificarCSV.Name = "btnModificarCSV";
+            this.btnModificarCSV.Size = new System.Drawing.Size(75, 23);
+            this.btnModificarCSV.TabIndex = 3;
+            this.btnModificarCSV.Text = "Modificar";
+            this.btnModificarCSV.UseVisualStyleBackColor = true;
+            this.btnModificarCSV.Click += new System.EventHandler(this.btnModificarCSV_Click);
+            // 
+            // dgvCSV
+            // 
+            this.dgvCSV.AllowUserToDeleteRows = false;
+            this.dgvCSV.AllowUserToOrderColumns = true;
+            this.dgvCSV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCSV.Location = new System.Drawing.Point(398, 13);
+            this.dgvCSV.Name = "dgvCSV";
+            this.dgvCSV.ReadOnly = true;
+            this.dgvCSV.Size = new System.Drawing.Size(533, 505);
+            this.dgvCSV.TabIndex = 5;
+            // 
+            // btnCrearCSV
+            // 
+            this.btnCrearCSV.Location = new System.Drawing.Point(987, 209);
+            this.btnCrearCSV.Name = "btnCrearCSV";
+            this.btnCrearCSV.Size = new System.Drawing.Size(75, 23);
+            this.btnCrearCSV.TabIndex = 1;
+            this.btnCrearCSV.Text = "Crear";
+            this.btnCrearCSV.UseVisualStyleBackColor = true;
+            this.btnCrearCSV.Click += new System.EventHandler(this.btnCrearCSV_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(987, 442);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // txtArchivo
+            // 
+            this.txtArchivo.Location = new System.Drawing.Point(12, 12);
+            this.txtArchivo.Multiline = true;
+            this.txtArchivo.Name = "txtArchivo";
+            this.txtArchivo.Size = new System.Drawing.Size(380, 506);
+            this.txtArchivo.TabIndex = 6;
+            // 
+            // frmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1126, 530);
+            this.Controls.Add(this.txtArchivo);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnCrearCSV);
+            this.Controls.Add(this.dgvCSV);
+            this.Controls.Add(this.btnModificarCSV);
+            this.Controls.Add(this.btnLeerCSV);
+            this.Name = "frmInicio";
+            this.Text = "Inicio";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCSV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnLeerCSV;
+        private System.Windows.Forms.Button btnModificarCSV;
+        private System.Windows.Forms.DataGridView dgvCSV;
+        private System.Windows.Forms.Button btnCrearCSV;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.TextBox txtArchivo;
+    }
+}
+
---
+++ CRUD CSV 2019/frmInicio.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CRUD_CSV_2019
+{
+    public partial class frmInicio : Form
+    {
+        public frmInicio()
+        {
+            InitializeComponent();
+            
+        }
+        private Stream myStream;
+        string line, rutaArchivo = string.Empty;
+        string[] valores;
+        bool archivoLeido = false;
+        private void BtnLeerCSV_Click(object sender, EventArgs e)
+        {
+            bool columnasLeidas = false;
+            dgvCSV.Rows.Clear();
+            dgvCSV.Columns.Clear();
+            OpenFileDialog fileDialog = new OpenFileDialog
+            {
+                Filter = "Archivos CSV (*.csv) | *.csv"
+            };
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                rutaArchivo = fileDialog.FileName;
+                try
+                {
+                    if((myStream=fileDialog.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            StreamReader file = new StreamReader(rutaArchivo);
+                            while((line = file.ReadLine()) != null)
+                            {
+                                valores = line.Split(';');//parte la linea del documento en un array por cada ';'
+                                if (columnasLeidas == false)
+                                {
+                                    foreach(string columnas in valores)//lee todo lo guardado en valores y lo guarda en columnas
+                                    {
+                                        DataGridViewTextBoxColumn dgvColumnas = new DataGridViewTextBoxColumn//se declara clase columna
+                                        {
+                                            HeaderText = columnas //el texto de la columna va a ser lo que se guardo en 'columnas' por el foreach
+                                        };
+                                        dgvCSV.Columns.Add(dgvColumnas);
+                                    }                                    
+                                    columnasLeidas=true;//despues de haber leido la primera linea, dejará de agregar las columnas 
+                                }
+                                else
+                                {
+                                    dgvCSV.Rows.Add(valores);//al estar en el while, va con la siguiente linea y agrega las filas
+                                }
+                                
+                            }                            
+                            file.Close();
+                        }
+                        StreamReader archivoLeido = new StreamReader(rutaArchivo);
+                        txtArchivo.Text = archivoLeido.ReadToEnd();
+                        archivoLeido.Close();
+                    }
+                }catch (Exception ex)
+                {
+                    MessageBox.Show("Error: No se pudo encontrar el archivo en el disco. Original error: " + ex.Message);
+                }
+                archivoLeido=true;
+            }
+        }
+
+        private void btnModificarCSV_Click(object sender, EventArgs e)
+        {
+            
+            if (archivoLeido == false)
+            {
+                OpenFileDialog fileDialog = new OpenFileDialog()
+                {
+                    Filter = "Archivos CSV (*.csv)| *.csv"
+                };
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    rutaArchivo = fileDialog.FileName;
+                    StreamReader file = new StreamReader(rutaArchivo);
+                    txtArchivo.Text = file.ReadToEnd();
+                    file.Close();
+                    archivoLeido = true;
+                }      
+            }
+            else
+            {
+                DialogResult alerta = MessageBox.Show("Modificará el ultimo archivo leído con exactamente lo que haya en la caja de texto.\rDesea continuar?", "Cuidado", MessageBoxButtons.OKCancel);
+                if(alerta == DialogResult.OK)
+                {
+                    StreamWriter streamWriter = new StreamWriter(rutaArchivo);
+                    streamWriter.Write(txtArchivo.Text);
+                    MessageBox.Show("Archivo modificado con exito");
+                    streamWriter.Close();
+                    txtArchivo.Clear();
+                }                
+            }           
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult alerta = MessageBox.Show("Eliminará el ultimo archivo leído con exactamente lo que haya en la caja de texto.\rDesea continuar?", "Cuidado", MessageBoxButtons.OKCancel);
+                if (alerta == DialogResult.OK)
+                {
+                    File.Delete(rutaArchivo);
+                    txtArchivo.Clear();
+                    MessageBox.Show("Archivo Eliminado con exito");
+                }
+            }catch(Exception error)
+            {
+                MessageBox.Show("Debe leer o modificar un archivo antes de eliminarlo. " + error.Message, "Recuerde");
+            }
+            
+        }
+
+        private void btnCrearCSV_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog
+            {
+                Filter = "Archivos CSV (*.csv)| *.csv"
+            };
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter streamWriter = new StreamWriter(saveDialog.FileName);
+                streamWriter.Write(txtArchivo.Text);    
+                MessageBox.Show("Archivo creado con exito");
+                streamWriter.Close();
+                txtArchivo.Clear();
+            }
+
+        }
+    }
+}
